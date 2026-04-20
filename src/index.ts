eb66b0ec06f814342cb16f6ced6ffdb43a898a40
Loris Rüegg <94956347+LorisRue@users.noreply.github.com>
2026-04-20 08:32:02 +0200

    setup yargs

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,25 @@
 
 // TODO: Add linting and script for linting 'npm run lint'
 
-console.log("Hello, World!");
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+yargs()
+  .scriptName("projgen")
+  .usage("$0 <command> [args]")
+  .command({
+    command: "create [templatePath]",
+    describe: "Create a new project from a template",
+    aliases: ["c", "cr"],
+    builder: (yargs) => {
+      return yargs.positional("templatePath", {
+        type: "string",
+        describe: "Path to the template file",
+      });
+    },
+    handler: (argv) => {
+      console.log("Creating project with template at", argv.templatePath);
+    },
+  })
+  .help()
+  .parse(hideBin(process.argv));
