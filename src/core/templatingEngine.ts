ed7919e2d538934e55ba65d85d1215e9e5252b29
Loris Rüegg <94956347+LorisRue@users.noreply.github.com>
2026-04-21 08:16:49 +0200

    Add inquirer dependency and implement variable prompting functions

--- src/core/templatingEngine.ts
+++ src/core/templatingEngine.ts
@@ -1,11 +1,41 @@
-import type { Template, Variable } from "../types/template";
+import type {
+  StringVariable,
+  NumberVariable,
+  Template,
+  Variable,
+  BooleanVariable,
+  SelectVariable,
+} from "../types/template";
 import type { Variable as VariableValue } from "../types/variable";
 
 export const scaffoldFromTemplate = (template: Template) => {
   const variables = promptForVariables(template.variables);
+
+  console.log(variables);
 };
 
-export const promptForVariables = (variables: Variable[]): VariableValue[] => {
+const promptForVariables = (variables: Variable[]): VariableValue[] => {
   // Implementation for prompting users for variable values
   return [];
+};
+
+const promptForString = (
+  variableInfo: StringVariable,
+): { name: string; content: string | null } => {
+  return { name: "temp", content: null };
+};
+const promptForNumber = (
+  variableInfo: NumberVariable,
+): { name: string; content: number | null } => {
+  return { name: "temp", content: null };
+};
+const promptForBoolean = (
+  variableInfo: BooleanVariable,
+): { name: string; content: boolean } => {
+  return { name: "temp", content: false };
+};
+const promptForSelect = (
+  variableInfo: SelectVariable,
+): { name: string; content: string | string[] | number | number[] | null } => {
+  return { name: "temp", content: null };
 };
--- src/types/variable.ts
+++ src/types/variable.ts
@@ -1,4 +1,4 @@
 export interface Variable {
   name: string; // The name of the variable by which it can be referenced later in the template
-  content: string | number | boolean | string[] | number[]; // The value of the variable
+  content: string | number | boolean | string[] | number[] | null; // The value of the variable
 }
