db44df18fb17792ffdff72b6de6314d770818f2f
Loris Rüegg <94956347+LorisRue@users.noreply.github.com>
2026-04-21 09:54:01 +0200

    Add functions for prompting

--- src/core/templatingEngine.ts
+++ src/core/templatingEngine.ts
@@ -1,41 +1,71 @@
-import type {
-  StringVariable,
-  NumberVariable,
-  Template,
-  Variable,
-  BooleanVariable,
-  SelectVariable,
-} from "../types/template";
+import type { Template, Variable } from "../types/template";
 import type { Variable as VariableValue } from "../types/variable";
+import prompter from "../utils/prompter.ts";
 
-export const scaffoldFromTemplate = (template: Template) => {
-  const variables = promptForVariables(template.variables);
-
+export const scaffoldFromTemplate = async (template: Template) => {
+  template.variables = [
+    {
+      name: "tempString",
+      type: "string",
+      message: "Enter a string variable",
+      required: true,
+    },
+    {
+      name: "tempNumber",
+      type: "number",
+      message: "Enter a number variable",
+      required: true,
+    },
+    {
+      name: "tempBoolean",
+      type: "boolean",
+      message: "Enter a boolean variable",
+    },
+    {
+      name: "tempSingleSelect",
+      type: "select",
+      message: "Select an option",
+      options: ["Option 1", "Option 2", "Option 3"],
+      multiple: false,
+      required: true,
+    },
+    {
+      name: "tempMultiSelect",
+      type: "select",
+      message: "Select multiple options",
+      options: ["Option 1", "Option 2", "Option 3"],
+      multiple: true,
+      required: true,
+    },
+  ];
+  const variables = await promptForVariables(template.variables);
   console.log(variables);
 };
 
-const promptForVariables = (variables: Variable[]): VariableValue[] => {
+const promptForVariables = async (
+  variables: Variable[],
+): Promise<VariableValue[]> => {
   // Implementation for prompting users for variable values
-  return [];
-};
-
-const promptForString = (
-  variableInfo: StringVariable,
-): { name: string; content: string | null } => {
-  return { name: "temp", content: null };
-};
-const promptForNumber = (
-  variableInfo: NumberVariable,
-): { name: string; content: number | null } => {
-  return { name: "temp", content: null };
-};
-const promptForBoolean = (
-  variableInfo: BooleanVariable,
-): { name: string; content: boolean } => {
-  return { name: "temp", content: false };
-};
-const promptForSelect = (
-  variableInfo: SelectVariable,
-): { name: string; content: string | string[] | number | number[] | null } => {
-  return { name: "temp", content: null };
+  let variableValues: VariableValue[] = [];
+  for (const variable of variables) {
+    switch (variable.type) {
+      case "string":
+        const value = await prompter.promptForString(variable);
+        variableValues.push(value);
+        break;
+      case "number":
+        const numValue = await prompter.promptForNumber(variable);
+        variableValues.push(numValue);
+        break;
+      case "boolean":
+        const boolValue = await prompter.promptForBoolean(variable);
+        variableValues.push(boolValue);
+        break;
+      case "select":
+        const selectValue = await prompter.promptForSelect(variable);
+        variableValues.push(selectValue);
+        break;
+    }
+  }
+  return variableValues;
 };
--- src/types/template.ts
+++ src/types/template.ts
@@ -33,14 +33,15 @@ export const numberVariableSchema = baseVariableSchema.safeExtend({
 export const booleanVariableSchema = baseVariableSchema.safeExtend({
   type: z.literal("boolean"), // The type of the variable, used to determine how to prompt the user for input
   default: z.boolean().optional(), // An optional default value for the variable
+  // Doesn't need a required field since it will always have a value (true or false)
 });
 
 // The Schema for a select variable
 export const selectVariableSchema = baseVariableSchema.safeExtend({
   type: z.literal("select"), // The type of the variable, used to determine how to prompt the user for input
-  required: z.boolean(), // Whether the user must provide a value when prompted
+  required: z.boolean(), // Whether the user must provide a value when prompted, is ignored for single selects (one must be selected there)
   options: z.array(z.union([z.string(), z.number()])), // An array of option for the select variable type, should only be provided if the variable type is "select", will be ignored otherwhise
-  multible: z.boolean().optional(), // Whether the user can select multiple options
+  multiple: z.boolean().optional(), // Whether the user can select multiple options
 });
 
 export const VariableSchema = z.discriminatedUnion("type", [
