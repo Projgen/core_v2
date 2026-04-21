62c27636c2b95e4dc3a57470859964fbc7da29cb
Loris Rüegg <94956347+LorisRue@users.noreply.github.com>
2026-04-21 10:29:35 +0200

    implement prompting for template variables

--- src/core/templatingEngine.ts
+++ src/core/templatingEngine.ts
@@ -1,68 +1,43 @@
 import type { Template, Variable } from "../types/template";
 import type { Variable as VariableValue } from "../types/variable";
-import prompter from "../utils/prompter.ts";
+import prompter, { type Prompter } from "../utils/prompter.ts";
 
 export const scaffoldFromTemplate = async (template: Template) => {
-  template.variables = [
-    {
-      name: "tempString",
-      type: "string",
-      message: "Enter a string variable",
-      required: true,
-    },
-    {
-      name: "tempNumber",
-      type: "number",
-      message: "Enter a number variable",
-      required: true,
-    },
-    {
-      name: "tempBoolean",
-      type: "boolean",
-      message: "Enter a boolean variable",
-    },
-    {
-      name: "tempSingleSelect",
-      type: "select",
-      message: "Select an option",
-      options: ["Option 1", "Option 2", "Option 3"],
-      multiple: false,
-      required: true,
-    },
-    {
-      name: "tempMultiSelect",
-      type: "select",
-      message: "Select multiple options",
-      options: ["Option 1", "Option 2", "Option 3"],
-      multiple: true,
-      required: true,
-    },
-  ];
+  printTemplateInfo(template);
   const variables = await promptForVariables(template.variables);
   console.log(variables);
 };
 
+const printTemplateInfo = (
+  template: Template,
+  logger: (message: string) => void = console.log,
+) => {
+  logger(`\n${template.name} #${template.version} - ${template.author}`);
+  logger(`${template.description}\n`);
+};
+
 const promptForVariables = async (
   variables: Variable[],
+  _prompter: Prompter = prompter,
 ): Promise<VariableValue[]> => {
   // Implementation for prompting users for variable values
   let variableValues: VariableValue[] = [];
   for (const variable of variables) {
     switch (variable.type) {
       case "string":
-        const value = await prompter.promptForString(variable);
+        const value = await _prompter.promptForString(variable);
         variableValues.push(value);
         break;
       case "number":
-        const numValue = await prompter.promptForNumber(variable);
+        const numValue = await _prompter.promptForNumber(variable);
         variableValues.push(numValue);
         break;
       case "boolean":
-        const boolValue = await prompter.promptForBoolean(variable);
+        const boolValue = await _prompter.promptForBoolean(variable);
         variableValues.push(boolValue);
         break;
       case "select":
-        const selectValue = await prompter.promptForSelect(variable);
+        const selectValue = await _prompter.promptForSelect(variable);
         variableValues.push(selectValue);
         break;
     }
